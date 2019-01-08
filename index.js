8ad28916ed0b7a35355a3698ed901889046b82f5
AlexBeck08 <44574067+AlexBeck08@users.noreply.github.com>
2019-01-07 18:03:58 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+// Change dice image #1
+document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+// Change dice image #2
+document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+// Changing heading depending on winner
+if (randomNumber1 > randomNumber2) {
+   document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+   document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+} else {
+   document.querySelector("h1").innerHTML = "Draw!";
+}
